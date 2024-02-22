d86718963afd600bd9c853cc513b80f7f58fc03e
Nsantg <nscarmona_170@cue.edu.co>
2024-02-22 06:38:31 -0500

    tarea 21/02/24

---
+++ ejemplo3.js
@@ -0,0 +1,5 @@
+let ocupacionCancha = true;
+let nombreCancha = "bernabeu";
+
+let mensaje = ocupacionCancha ? `la cancha llamada ${nombreCancha} esta ocupada"` : `la cancha llamada ${nombreCancha} esta disponible"`;
+console.log(mensaje);
